74386df163fe8835c6921b1dc36730a37c97efe7
ybentz <ybentzur7@gmail.com>
2018-10-27 22:00:24 -0700

    added initial server + DB setup

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,19 @@
-// let's go!
+require('dotenv').config({ path: '.env' })
+const createServer = require('./createServer')
+const db = require('./db')
+
+const server = createServer()
+
+// TODO use express middleware to handle cookies (JWT)
+// TODO use express middleware to populate current user
+
+const { FRONTEND_URL } = process.env
+
+server.start({
+  cors: {
+    credentials: true,
+    origin: FRONTEND_URL,
+  },
+}, deets => {
+  console.log(`Server now running on port http://localhost:${deets.port}`)
+})
