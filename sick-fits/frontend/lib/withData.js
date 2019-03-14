c2c3ebd797e568d3f4264a2aa6dd447e57c5979e
ybentz <ybentzur7@gmail.com>
2019-03-13 23:14:57 -0700

    added cart open/close functionality - using apollo local state query/mutation

--- sick-fits/frontend/lib/withData.js
+++ sick-fits/frontend/lib/withData.js
@@ -2,17 +2,41 @@ import withApollo from 'next-with-apollo';
 import ApolloClient from 'apollo-boost';
 import { endpoint } from '../config';
 
+import { LOCAL_STATE_QUERY } from '../components/Cart';
+
 function createClient({ headers }) {
   return new ApolloClient({
     uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
     request: operation => {
       operation.setContext({
         fetchOptions: {
-          credentials: 'include',
+          credentials: 'include'
         },
-        headers,
+        headers
       });
     },
+    // local data - use GraphQL for local state management
+    clientState: {
+      resolvers: {
+        Mutation: {
+          // first argument unknown and unused lol
+          toggleCart(_, variables, { cache }) {
+            // read cartOpen value from cache
+            const { cartOpen } = cache.readQuery({
+              query: LOCAL_STATE_QUERY
+            });
+            const data = {
+              data: { cartOpen: !cartOpen }
+            };
+            cache.writeData(data);
+            return data;
+          }
+        }
+      },
+      defaults: {
+        cartOpen: true
+      }
+    }
   });
 }
 
