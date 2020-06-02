530282f0466052985acfcf6547d3b60306b8e4f0
Yonatan Bentzur <ybentzur7@gmail.com>
2020-06-02 15:03:02 -0700

    Added useAutofillForm hook to handle Chrome autofill that doesn't trigger a change event

---
+++ sick-fits/frontend/lib/useAutofillForm.ts
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react'
+
+import useForm from './useForm'
+
+type NamedRef = {
+  name: string
+  ref: React.MutableRefObject<HTMLInputElement | HTMLTextAreaElement>
+}
+
+// Handles forms that use Chrome's autofill (NOT autocomplete) to get the filled values
+// from the DOM
+function useAutofillForm(
+  initial: FormInputState = {},
+  autofillNamedRefs: NamedRef[] = []
+) {
+  const { inputs, handleChange, resetForm, setInputsUnsafe } = useForm(initial)
+  useEffect(() => {
+    if (!autofillNamedRefs.length) return
+    const interval = setInterval(() => {
+      if (autofillNamedRefs[0].ref.current) {
+        const inputs = Object.fromEntries(
+          autofillNamedRefs.map((namedRef) => {
+            const { name, ref } = namedRef
+            return [name, ref.current.value || '']
+          })
+        )
+        clearInterval(interval)
+        setInputsUnsafe(inputs)
+      }
+    }, 100)
+    return () => clearInterval(interval)
+  }, [])
+
+  return {
+    inputs,
+    handleChange,
+    resetForm,
+  }
+}
+
+export default useAutofillForm
